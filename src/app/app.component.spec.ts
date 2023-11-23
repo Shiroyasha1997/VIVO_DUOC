532fceeabd828b367c5d60c61a5afda32437b50b
Shiroyasha1997 <valenzueladiego1997@gmail.com>
2023-11-23 10:49:49 -0300

    Se agregaron Pruebas con Karma Jasmine

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,36 @@
+import { Usuario } from './model/usuario';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { activeAnimations } from '@ionic/core/dist/types/utils/overlays';
 import { AppComponent } from './app.component';
 
-describe('AppComponent', () => {
-  it('should create the app', () => {
-    TestBed.overrideComponent(AppComponent, {
-      add: {
-        imports: [RouterTestingModule]
-      }
-    });
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+describe('Probar al comiezo de la aplicacion', () => {
+
+  function suma(x: number, y:number) {
+    return x + y;
+  }
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [],
+      imports: [AppComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    }).compileComponents();
   });
+  
+    it('Se deberia crear la aplicacion', () => {
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.componentInstance;
+      expect(app).toBeTruthy();
+    });
+
+    it('Probar que el titulo de la aplicacion sea "Asistencia DUOC"', () => {
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.componentInstance;
+      expect(app.title).toEqual('Asistencia DUOC');
+    });
+
+    it('Probar la funcion suma donde 10+20=30', () => {
+      expect(suma(10, 20)).toEqual(30);
+    });
 });
